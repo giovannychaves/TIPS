0ea4179db1260596f374b1ac4a609e6e50b432dd
Giovanny Chaves <giovanny.chaves@enext.com.br>
2019-10-23 16:59:43 -0300

    ajustes

--- src/js/BR.js
+++ src/js/BR.js
@@ -4,12 +4,10 @@ const outputBr = document.querySelector('.outputBr');
 
     function mascaraBr() {
         let numeroLimpo = numero.value.replace(/\D/g, '');
-        if(numeroLimpo.length === 10){
-            outputBr.innerHTML += numeroLimpo.replace(/(\d{2})(\d{4})(\d{4})/,"(\$1) \$2-\$3");
-        }else{
-            outputBr.innerHTML += numeroLimpo.replace(/(\d{2})(\d{5})(\d{4})/,"(\$1) \$2-\$3");
+        numeroLimpo.length === 10 ? 
+        outputBr.innerHTML += numeroLimpo.replace(/(\d{2})(\d{4})(\d{4})/,"(\$1) \$2-\$3") : 
+        outputBr.innerHTML += numeroLimpo.replace(/(\d{2})(\d{5})(\d{4})/,"(\$1) \$2-\$3");
         }
-    };
 
 
 btnBr.addEventListener('click', mascaraBr);
